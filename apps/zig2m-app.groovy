bdd3115a8d0677e47b8c2e60d54ff1144e7a8a26
damunn <dmunn2@gmail.com>
2025-08-21 20:19:53 -0500

    Update zig2m-app.groovy
    Change namespace and move child drivers under broker.

--- apps/zig2m-app.groovy
+++ apps/zig2m-app.groovy
@@ -1,7 +1,7 @@
 /**
  * ====================  Zigbee2MQTT Connect - Zigbee2MQTT Integration ==================
  *
- *  Copyright 2021 Robert Morris
+ *  Copyright 2025 Dale Munn
  *
  *  DESCRIPTION:
  *  Community-developed integration for importing Zigbee2MQTT devices into Hubitat via an
@@ -21,9 +21,10 @@
  *
  * =======================================================================================
  *
- *  Last modified: 2021-12-12
+ *  Last modified: 2025-08-11
  * 
  *  Changelog:
+ *  v1.0    - Chage to child devices under broker
  *  v0.4    - (Beta) More driver matches, app to/from broker setting updates, etc.
  *  v0.1    - (Beta) Initial Public Release
  */ 
@@ -33,13 +34,13 @@ import hubitat.helper.ColorUtils
 import groovy.transform.Field
 
 @Field static final Integer debugAutoDisableSeconds = 1800
-@Field static final String customDriverNamespace = "RMoRobert"
+@Field static final String customDriverNamespace = "Zigbee2MQTT"
 @Field static final String stockDriverNamespace = "hubitat"
 
 definition (
    name: "Zigbee2MQTT Connect",
-   namespace: "RMoRobert",
-   author: "Robert Morris",
+   namespace: "Zigbee2MQTT",
+   author: "Robert Morris/Dale Munn",
    description: "Integrate devices from Zigbee2MQTT (connects to MQTT broker)",
    category: "Convenience",
    installOnOpen: true,
@@ -186,7 +187,7 @@ void createNewSelectedDevices() {
          if (enableDebug) log.debug "Creating new device for IEEE ${ieee})"
          String devDNI = "Zig2M/${app.id}/${ieee}"
          Map z2mDev = zigDevs.find { it.ieee_address == ieee }
-         DeviceWrapper cd = getChildDevice(devDNI)
+         DeviceWrapper cd = brokerDev.brokerChildDevice(devDNI)
          if (cd != null) {
             if (enableDebug) log.debug "not creating device for $ieee; already exists"
          }
@@ -195,7 +196,7 @@ void createNewSelectedDevices() {
                if (enableDebug) log.debug "Creating device for IEEE = $ieee, name = ${z2mDev.friendly_name}"
                def (String driverName, String namespace) = getBestMatchDriver(z2mDev.definition.exposes)
                try {
-                  DeviceWrapper d = addChildDevice(namespace, driverName, devDNI, [name: z2mDev.friendly_name])
+                  DeviceWrapper d = brokerDev.brokerAddChildDevice(namespace, driverName, devDNI, [name: z2mDev.friendly_name])
                   if (d != null) {
                      if (z2mDev.definition?.vendor) d.updateDataValue("vendor", z2mDev.definition.vendor)
                      if (z2mDev.definition?.model) d.updateDataValue("model", z2mDev.definition.model)
@@ -232,34 +233,40 @@ List<String> getBestMatchDriver(List<Map> exposes) {
    String driverName
    if (exposes.find { it.name == "occupancy"}) {
       if (exposes.find { it.name == "temperature"} && exposes.find { it.name == "humidity"} ) {
-         driverName = "Generic Component Motion/Temperature/Humidity Sensor"
-         namespace = customDriverNamespace
+         driverName = "Generic Component Motion Temperature Humidity Sensor"
+  //       namespace = customDriverNamespace
       }
       else if (exposes.find { it.name == "temperature"} && exposes.find { it.name == "illuminance_lux"} ) {
-         driverName = "Generic Component Motion/Temperature/Lux Sensor"
-         namespace = customDriverNamespace
+         driverName = "Generic Component Motion Temperature/Lux Sensor"
+  //       namespace = customDriverNamespace
       }
       else if (exposes.find { it.name == "temperature"} ) {
-         driverName = "Generic Component Motion/Temperature Sensor"
-         namespace = customDriverNamespace
+         driverName = "Generic Component Motion Temperature Sensor"
+  //       namespace = customDriverNamespace
       }
       else if (exposes.find { it.name == "battery"} ) {
          driverName = "Generic Component Motion (with Battery) Sensor"
-         namespace = customDriverNamespace
+  //       namespace = customDriverNamespace
       }
       else {
          driverName = "Generic Component Motion Sensor"
       }
    }
-   else if (exposes.find {it.name == "contact" && it.name == "x_axis"}) {
-      driverName = "Generic Component Acceleration/Axis/Contact Sensor"
-      namespace = customDriverNamespace
-   }
    else if (exposes.find {it.name == "contact" }) {
-      driverName = "Generic Component Contact Sensor"
+      if(exposes.find {it.name == "x_axis"}) {
+         driverName = "zig2m Component Acceleration/Axis/Contact Sensor"
+         namespace = customDriverNamespace
+      }
+      else if (exposes.find{it.name="battery"} ) {
+         driverName = "zig2m Component Contact Sensor"
+          namespace = customDriverNamespace
+      }
+      else  {
+         driverName = "Generic Component Contact Sensor"   
+      }
    }
    else if (exposes.find {it.name == "temperature" }) { // should break out temp-only sensor, but most seem to do both so not a huge priority...
-      driverName = "Generic Component Temperature/Humidity Sensor"
+      driverName = "zig2m Component Temperature Humidity Sensor"
       namespace = customDriverNamespace
    }
    else if (exposes.features.find { flist -> flist.find { f -> f.name == "color_xy"  || f.name == "color_hs "} &&
@@ -285,8 +292,12 @@ List<String> getBestMatchDriver(List<Map> exposes) {
    else if (exposes.features.find { flist -> flist.find { f -> f.name == "state"  } } ) {
       driverName = "Generic Component Switch"
    }
+   else if (exposes.find { it.name == "water_leak" }) {
+      driverName = "zig2m Component Water Sensor"
+      namespace = customDriverNamespace
+   }
    else {
-      driverName = "Zigbee2MQTT Generic Device"
+      driverName = "zig2m Generic Device"
       namespace = customDriverNamespace
    }
    return [driverName, namespace]
@@ -294,7 +305,9 @@ List<String> getBestMatchDriver(List<Map> exposes) {
 
 Map pageManage() {
    createNewSelectedDevices()
-   List<String> cdNames = getChildDevices().findAll { DeviceWrapper cd ->
+   DeviceWrapper brokerDev = getChildDevice("Zig2M/${app.id}")
+ 
+   List<String> cdNames = brokerDev.brokerGetChildDevices().findAll { DeviceWrapper cd ->
       cd.deviceNetworkId != "Zig2M/${app.id}" }.collect {
          DeviceWrapper cd -> cd.displayName
    }
@@ -324,7 +337,7 @@ Map pageSelectDevices(params) {
       allZig2MDevs = brokerDev.getDeviceList()
       allZig2MDevs.each {
          if (it.type?.toLowerCase() != "coordinator") {
-            DeviceWrapper cd = getChildDevice("Zig2M/${app.id}/${it.ieee_address}")
+            DeviceWrapper cd = brokerDev.brokerChildDevice("Zig2M/${app.id}/${it.ieee_address}")
             if (cd != null) {
                claimedDevs << cd
             }
@@ -333,9 +346,11 @@ Map pageSelectDevices(params) {
             }
          }
       }
-      abandonedDevs = getChildDevices()
-      abandonedDevs.removeAll { it.deviceNetworkId in claimedDevs.collect { it.deviceNetworkId } }
-      abandonedDevs.removeAll { it.deviceNetworkId == "Zig2M/${app.id}" }
+	   log.debug "claimedDevs=$claimedDevs"
+      abandonedDevs = brokerDev.brokerGetChildDevices()
+      if(abandonedDevs != null) {
+         abandonedDevs.removeAll { it.deviceNetworkId in claimedDevs.collect { it.deviceNetworkId } }
+         }
    }
    else {
       log.warn "Zigbee2MQTT broker device not found!"
@@ -426,216 +441,4 @@ private void logDebug(String str) {
    if (settings.enableDebug != false) log.debug(str)
 }
 
-////////////////////////////////////
-// Component Methods
-////////////////////////////////////
 
-void componentRefresh(DeviceWrapper device) {
-   if (enableDebug) log.debug "componentRefresh(${device.displayName})"
-   DeviceWrapper brokerDev = getChildDevice("Zig2M/${app.id}")
-   String ieee = device.getDeviceNetworkId().tokenize('/')[-1]
-   List<Map<String,String>> payloads = []
-   if (device.hasAttribute("switch")) payloads << [state: ""]
-   if (device.hasAttribute("level")) payloads << [brightness: ""]
-   if (device.hasAttribute("hue")) payloads << [color: [x: "", y: ""]]
-   if (device.hasAttribute("colorTemperature")) payloads << [color_temp: ""]
-   if (device.hasAttribute("lock")) payloads << [state: ""]
-   // probably can flesh this out more for other devices later...
-   payloads.each { Map<String,String> payload ->
-      brokerDev.publishForIEEE(ieee, "get", payload)
-      pauseExecution(50)
-   }
-}
-
-void componentOn(DeviceWrapper device) {
-   if (enableDebug) log.debug "componentOn(${device.displayName})"
-   DeviceWrapper brokerDev = getChildDevice("Zig2M/${app.id}")
-   String ieee = device.getDeviceNetworkId().tokenize('/')[-1]
-   Map<String,String> payload = [state: "ON"]
-   brokerDev.publishForIEEE(ieee, "set", payload)
-}
-
-void componentOff(DeviceWrapper device) {
-   if (enableDebug) log.debug "componentOn(${device.displayName})"
-   DeviceWrapper brokerDev = getChildDevice("Zig2M/${app.id}")
-   String ieee = device.getDeviceNetworkId().tokenize('/')[-1]
-   Map<String,String> payload = [state: "OFF"]
-   brokerDev.publishForIEEE(ieee, "set", payload)
-}
-
-void componentSetLevel(DeviceWrapper device, Number level, Number transitionTime=null) {
-   if (enableDebug) log.debug "componentSetLevel(${device.displayName}, $level, $transitionTime)"
-   DeviceWrapper brokerDev = getChildDevice("Zig2M/${app.id}")
-   String ieee = device.getDeviceNetworkId().tokenize('/')[-1]
-   Map<String,Number> payload = [brightness: Math.round((level as Float) * 2.55)]
-   if (transitionTime != null) payload << [transition: transitionTime]
-   brokerDev.publishForIEEE(ieee, "set", payload)
-}
-
-/* Haven't found Z2M bulb that supports yet...
-void componentPresetLevel(DeviceWrapper device, Number level) {
-   if (enableDebug) log.debug "componentPresetLevel(${device.displayName}, $level)"
-   DeviceWrapper brokerDev = getChildDevice("Zig2M/${app.id}")
-   String ieee = device.getDeviceNetworkId().tokenize('/')[-1]
-   Map<String,Number> payload = [brightness: Math.round((level as Float) * 2.55)]
-   brokerDev.publishForIEEE(ieee, "set", payload)
-}
-*/
-
-void componentStartLevelChange(DeviceWrapper device, String direction) {
-   if (enableDebug) log.debug "componentStartLevelChange(${device.displayName}, $direction)"
-   DeviceWrapper brokerDev = getChildDevice("Zig2M/${app.id}")
-   String ieee = device.getDeviceNetworkId().tokenize('/')[-1]
-   Integer mvRate = (direction.toLowerCase() == "up" ? 85 : -85)
-   Map<String,Number> payload = [brightness_move: mvRate]
-   brokerDev.publishForIEEE(ieee, "set", payload)
-}
-
-void componentStopLevelChange(DeviceWrapper device) {
-   if (enableDebug) log.debug "componentStopLevelChange(${device.displayName})"
-   DeviceWrapper brokerDev = getChildDevice("Zig2M/${app.id}")
-   String ieee = device.getDeviceNetworkId().tokenize('/')[-1]
-   Map<String,Number> payload = [brightness_move: 0]
-   brokerDev.publishForIEEE(ieee, "set", payload)
-}
-
-void componentSetColorTemperature(DeviceWrapper device, Number colorTemperature, Number level=null, Number transitionTime=null) {
-   if (enableDebug) log.debug "componentSetColorTemperature(${device.displayName}, $colorTemperature, $level, $transitionTime)"
-   DeviceWrapper brokerDev = getChildDevice("Zig2M/${app.id}")
-   String ieee = device.getDeviceNetworkId().tokenize('/')[-1]
-   Map<String,Number> payload = [color_temp: Math.round(1000000.0/colorTemperature)]
-   if (level != null) payload << [brightness: Math.round((level as Float) * 2.55)]
-   if (transitionTime != null) payload << [transition: transitionTime]
-   if (device.currentValue("switch") != "on") payload << [state: "ON"]
-   brokerDev.publishForIEEE(ieee, "set", payload)
-}
-
-// Uses RGB (most widely accepted?)
-void componentSetColor(DeviceWrapper device, Map<String,Number> colorMap) {
-   if (enableDebug) log.debug "componentSetColor(${device.displayName}, $colorMap)"
-   DeviceWrapper brokerDev = getChildDevice("Zig2M/${app.id}")
-   String ieee = device.getDeviceNetworkId().tokenize('/')[-1]
-   List rgb = hubitat.helper.ColorUtils.hsvToRGB([colorMap.hue, colorMap.saturation, colorMap.level ?: device.currentValue('level')])
-   Map<String,Map<String,String>> payload = [color: [rgb: rgb.join(',')]]
-   if (colorMap.rate != null) payload << [transition: colorMap.rate]
-   if (device.currentValue("switch") != "on") payload << [state: "ON"]
-   brokerDev.publishForIEEE(ieee, "set", payload)
-}
-
-// Uses HS (doesn't work for all?)
-void componentSetColorHS(DeviceWrapper device, Map<String,Number> colorMap) {
-   if (enableDebug) log.debug "componentSetColor(${device.displayName}, $colorMap)"
-   DeviceWrapper brokerDev = getChildDevice("Zig2M/${app.id}")
-   String ieee = device.getDeviceNetworkId().tokenize('/')[-1]
-   Map<String,Map<String,String>> payload = [color: [h: Math.round((colorMap.hue as float) / 3.60), s: colorMap.saturation, v: colorMap.level ?: device.currentValue('level')]]
-   if (colorMap.rate != null) payload << [transition: colorMap.rate]
-   if (device.currentValue("switch") != "on") payload << [state: "ON"]
-   brokerDev.publishForIEEE(ieee, "set", payload)
-}
-
-void componentSetHue(DeviceWrapper device, Number hue) {
-   if (enableDebug) log.debug "componentSetHue(${device.displayName}, $hue)"
-   DeviceWrapper brokerDev = getChildDevice("Zig2M/${app.id}")
-   String ieee = device.getDeviceNetworkId().tokenize('/')[-1]
-   List rgb = hubitat.helper.ColorUtils.hsvToRGB([hue, device.currentValue('saturation'), device.currentValue('level')])
-   Map<String,Map<String,String>> payload = [color: [rgb: rgb.join(',')]]
-   if (device.currentValue("switch") != "on") payload << [state: "ON"]
-   brokerDev.publishForIEEE(ieee, "set", payload)
-}
-
-void componentSetSaturation(DeviceWrapper device, Number sat) {
-   if (enableDebug) log.debug "componentSetSaturation(${device.displayName}, $sat)"
-   DeviceWrapper brokerDev = getChildDevice("Zig2M/${app.id}")
-   String ieee = device.getDeviceNetworkId().tokenize('/')[-1]
-   List rgb = hubitat.helper.ColorUtils.hsvToRGB([device.currentValue('hue'), sat, device.currentValue('level')])
-   Map<String,Map<String,String>> payload = [color: [rgb: rgb.join(',')]]
-   if (device.currentValue("switch") != "on") payload << [state: "ON"]
-   brokerDev.publishForIEEE(ieee, "set", payload)
-}
-
-// Hubitat uses number, but String is easier to work with in Z2M, so custom
-// bulb driver implements both. Use the String variant only when calling parent for now!
-void componentSetEffect(DeviceWrapper device, String effectName) {
-   if (enableDebug) log.debug "componentSetEffect(${device.displayName}, String $effectName)"
-   DeviceWrapper brokerDev = getChildDevice("Zig2M/${app.id}")
-   String ieee = device.getDeviceNetworkId().tokenize('/')[-1]
-   Map<String,Number> payload = [effect: effectName]
-   brokerDev.publishForIEEE(ieee, "set", payload)
-}
-
-void componentSetEffect(DeviceWrapper device, Number effectNumber) {
-   if (enableDebug) log.debug "componentSetEffect(${device.displayName}, Number $effectNumber)"
-   log.warn "Not yet implemented; use String effecet name instead of number for now."
-}
- 
-void componentPublish(DeviceWrapper device, String topic=null, String payload=null) {
-   if (enableDebug) log.debug "componentPublish(${device.displayName}, $topic, $payload)"
-   DeviceWrapper brokerDev = getChildDevice("Zig2M/${app.id}")
-   String ieee = device.getDeviceNetworkId().tokenize('/')[-1]
-   brokerDev.publishForIEEE(ieee, topic, payload)
-} 
-
-void componentLock(DeviceWrapper device) {
-   if (enableDebug) log.debug "componentLock(${device.displayName})"
-   DeviceWrapper brokerDev = getChildDevice("Zig2M/${app.id}")
-   String ieee = device.getDeviceNetworkId().tokenize('/')[-1]
-   Map<String,String> payload = [state: "LOCK"]
-   brokerDev.publishForIEEE(ieee, "set", payload)
-}
-
-void componentUnlock(DeviceWrapper device) {
-   if (enableDebug) log.debug "componentUnlock(${device.displayName})"
-   DeviceWrapper brokerDev = getChildDevice("Zig2M/${app.id}")
-   String ieee = device.getDeviceNetworkId().tokenize('/')[-1]
-   Map<String,String> payload = [state: "UNLOCK"]
-   brokerDev.publishForIEEE(ieee, "set", payload)
-}
-
-void componentDeleteCode(DeviceWrapper device, Integer codePosition) {
-   if (enableDebug) log.debug "componentDeleteCode(${device.displayName}, ${codePosition})"
-   DeviceWrapper brokerDev = getChildDevice("Zig2M/${app.id}")
-   String ieee = device.getDeviceNetworkId().tokenize('/')[-1]
-   Map<String,String> payload = [pin_code: [user: codePosition, user_enabled: false, pin_code: null]]
-   brokerDev.publishForIEEE(ieee, "set", payload)
-}
-
-void componentGetCodes(DeviceWrapper device) {
-   if (enableDebug) log.debug "componentDeleteCode(${device.displayName}, ${codePosition})"
-   log.warn "getCodes() not implemented"
-}
-
-void componentSetCode(DeviceWrapper device, Integer codePosition, String pincode, String name=null) {
-   if (enableDebug) log.debug "componentSetCode(${device.displayName}, ${codePosition})"
-   DeviceWrapper brokerDev = getChildDevice("Zig2M/${app.id}")
-   String ieee = device.getDeviceNetworkId().tokenize('/')[-1]
-   Map<String,String> payload = [pin_code: [user: codePosition, user_enabled: true, pin_code: Integer.parseInt(pincode)]]
-   brokerDev.publishForIEEE(ieee, "set", payload)
-}
-
-void componentSetCodeLength(DeviceWrapper device, Integer codeLength) {
-   if (enableDebug) log.debug "componentSetCodeLength(${device.displayName}, ${codeLength})"
-   List<Map> evts = [[name: "codeLength", value: codeLength]]
-   device.parse(evts)
-}
-
-String getDefinitionForDevice(DeviceWrapper device) {
-   if (enableDebug) log.debug "getDefinitionForDevice(${device.displayName})"
-   DeviceWrapper brokerDev = getChildDevice("Zig2M/${app.id}")
-   String ieee = device.getDeviceNetworkId().tokenize('/')[-1]
-   if (brokerDev != null) {
-      List zigDevs = brokerDev.getDeviceList()
-      Map z2mDev = zigDevs.find { it.ieee_address == ieee }
-      if (z2mDev != null) {
-         // Can use if want Groovy toString output instead:
-         //log.debug "DEFINITION: ${z2mDev.definition}"
-         log.debug "DEFINITION: " + groovy.json.JsonOutput.prettyPrint(groovy.json.JsonOutput.toJson(z2mDev.definition))
-      }
-      else {
-         log.debug "No device found on Zigbee2MQTT broker for ${device.displayName}"
-      }
-   }
-   else {
-      log.warn "Zigbee2MQTT Broker device not found!"
-   }
-   return 
-}
